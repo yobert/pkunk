9fb31379ece955953f62279de9dee49b733fd313
Joel Jensen <yobert@gmail.com>
2014-09-12 02:47:31 -0600

    WIP messing about with deleting

--- examples/todo/js/list.jsx
+++ examples/todo/js/list.jsx
@@ -13,8 +13,10 @@ var List = React.createClass({
 	render: function() {
 		var list = [];
 
+		var t = this;
+
 		array_each(this.state.data_list, function(v, k) {
-			var cls = "list_item";
+			var cls = "touch list_item";
 			if(v.item_done)
 				cls += " list_item_done";
 
@@ -25,14 +27,21 @@ var List = React.createClass({
 						var dialog = jsb_ui_new_dialog({'title':'Edit Todo', 'center':true, 'onclose':function() {
 							React.unmountComponentAtNode(body);
 							return true;
-						}}, body, ['Close']);
+						}}, body, ['Close', {'value':'Delete', 'onclick':function(){
+							// delete the thing!
+							var payload = {'_delete':true, '_local_id':v._local_id};
 
-						React.renderComponent(Item({'initial':v}), body);
-						jsb_ui_center_dialog(dialog);
+							ps.pub('data_' + t.dclass + '_' + v._local_id, payload);
+							ps.pub('datalist_' + t.dclass, payload);
 
+							return false;
+						}}]);
+
+					React.renderComponent(Item({'initial':v}), body);
+					jsb_ui_center_dialog(dialog);
 
 				}}>
-					Item {v._local_id}: <b>"{v.item_title}"</b>
+				Item {v._local_id}: <b>"{v.item_title}"</b>
 				</div>
 			);
 		});
@@ -45,7 +54,9 @@ var List = React.createClass({
 						var dialog = jsb_ui_new_dialog({'title':'Add Todo', 'center':true, 'onclose':function() {
 							React.unmountComponentAtNode(body);
 							return true;
-						}}, body, ['Close']);
+						}}, body, ['Close', {'value':'Delete', 'onclick':function(){
+							return true;
+						}}]);
 
 						React.renderComponent(Item({}), body);
 						jsb_ui_center_dialog(dialog);
