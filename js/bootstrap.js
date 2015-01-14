4b3dd8d7ab7a01611a2a837244d2ff2e595058a5
Joel Jensen <yobert@gmail.com>
2015-01-14 15:37:23 -0700

    Add back bootstrap js helper code, Fix up static example

---
+++ js/bootstrap.js
@@ -0,0 +1,32 @@
+(function() {
+	window.bootstrap = bootstrap;
+	window.register_url = register_url;
+	window.current_path = current_path;
+
+	var url_handlers = {};
+
+	function bootstrap() {
+		var path = current_path();
+		var u = url_handlers[path];
+		if (!u)
+			throw ('no url_handler for path: "' + path + '"');
+
+		u.handler();
+	}
+
+	function register_url(url, dispatcher, handler) {
+		url_handlers[url] = {
+			'dispatcher': dispatcher,
+			'handler': handler
+		};
+	}
+
+	function current_path() {
+		var path = window.location.pathname;
+		if (path == "")
+			path = "/";
+
+		return path;
+	}
+
+})();
