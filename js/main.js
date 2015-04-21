a5fdd9f349f10f385b40ac2714177fbf84d885a1
Joel Jensen <joel.jensen@experticity.com>
2015-04-21 12:35:00 -0600

    Add package.json so the javascript can be packaged separately into NPM

---
+++ js/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+	ajax: require('./ajax'),
+	append: require('./append'),
+	bootstrap: require('./bootstrap'),
+	cookies: require('./cookies'),
+	dates: require('./dates'),
+	tag: require('./tag'),
+	tools: require('./tools'),
+	ui: {
+		dialog: require('./ui/dialog')
+	}
+};
+
